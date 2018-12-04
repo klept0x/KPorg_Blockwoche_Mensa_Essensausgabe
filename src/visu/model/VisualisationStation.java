1a92f56093235de4334ae3baaa4c389ee1699e48
kl3pt0x <kl3pt0x@gmail.com>
2018-12-04 21:37:24 +0100

    Singleton MensaStation - maxCashregister nicht mehr final - clone() hinzugefügt - clone() wirft exception NotCloneable JUnit Test für MensaStation - EcxeptionNotCloneableTest JavaDoc XML Test entfernt JUnit TheObject entfernt

--- src/visu/model/VisualisationStation.java
+++ src/visu/model/VisualisationStation.java
@@ -4,16 +4,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * Class for a visualisation  label
+ *
+ * @author Team5
+ * @version 1
+ */
 public class VisualisationStation {
 
-    private String station;
+    /** name of the visualisation label */
+    private String label;
+
+    /** idle time of the visualisation label */
     private int idelTime;
+
+    /** in use time of the visualisation label */
     private int inUseTime;
 
+    /** a list of the visualisation label */
     private static ArrayList<VisualisationStation> allVisualisationStations = new ArrayList<VisualisationStation>();
 
-    private VisualisationStation(String station, int idelTime, int inUseTime) {
-        this.station = station;
+    /**
+     * (private!) Constructor, creates a new visualisation label
+     *
+     * @param label    of the visualisation label
+     * @param idelTime   the idel time of the visualisation label
+     * @param inUseTime  the time that the visualisation label was be used
+     */
+    private VisualisationStation(String label, int idelTime, int inUseTime) {
+        this.label = label;
         this.idelTime = idelTime;
         this.inUseTime = inUseTime;
 
@@ -21,30 +40,62 @@ public class VisualisationStation {
 
     }
 
-    public static void create(String station, int idelTime, int inUseTime) {
-        new VisualisationStation(station,idelTime,inUseTime);
+    /**
+     * create a new visualisation label and add it to the visualisationstation list
+     *
+     * @param label    of the visualisation label
+     * @param idelTime   the idel time of the visualisation label
+     * @param inUseTime  the time that the visualisation label was be used
+     */
+    public static void create(String label, int idelTime, int inUseTime) {
+        new VisualisationStation(label,idelTime,inUseTime);
     }
 
-    public String getStation() {
-        return station;
+    /**
+     * Gets the name of the visualisation staion
+     *
+     * @return the name of the visualisation staion
+     */
+    public String getLabel() {
+        return label;
     }
 
+
+    /**
+     * Gets the idle time of the visualisation staion
+     *
+     * @return the idle time of the visualisation staion
+     */
     public int getIdelTime() {
         return idelTime;
     }
 
+    /**
+     * Get the in use time of the visualisation staion
+     *
+     * @return the in use time of the visualisation staion
+     */
     public int getInUseTime() {
         return inUseTime;
     }
 
+    /**
+     * sorts the visualisation staions in the list, so the poitns (poitns equals the visualisation staion) in the
+     * graph plotter are shown in descending order
+     */
     private static void sortiereVisualisirungStation(){
         Collections.sort(allVisualisationStations, Comparator.comparingInt(VisualisationStation::getInUseTime).reversed());
-
     }
 
+    /**
+     * Gets the visualisation staions in descending order
+     *
+     * @return the visualisation staions in descending order
+     */
     public static ArrayList<VisualisationStation> getAllVisualisationStations() {
+        //bad code example
+        //getter should just get the information
         sortiereVisualisirungStation();
-
         return allVisualisationStations;
     }
 }
